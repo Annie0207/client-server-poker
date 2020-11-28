2790c8308bebcaf09209a1fccd1ee799534306a8
eldss <evanldouglass@gmail.com>
2020-11-27 17:02:58 -0800

    Add comments to client/server

--- poker_client.py
+++ poker_client.py
@@ -3,3 +3,22 @@ The `poker_client` provides business logic to allow a client (player) to
 communicate with the poker server (game manager) and play a game of poker with
 others.
 '''
+
+def main():
+    # Parse command line args & validate
+    # Get server response for `start` or `join`
+    # If successful, set name and wait for other players to join until game starts
+    # Send Ante, if enough in wallet
+    # Get cards
+    # Allow player to swap cards until ready (set time limit (1 min?))
+    # Wait until this player's turn, print notifications of other players' choices
+    # On turn, get player choices until betting option chosen (check only allowed by first player to go)
+    # Repeat ^ until betting round over
+    # Choose cards to trade in, if any (set time limit (1 min?))
+    # Second round of betting (unless round ended in last bet by all players folding but one)
+    # If still in game, send hand to server for evaluation
+    # Get game result
+    pass
+
+if __name__ == '__main__':
+    main()
--- poker_server.py
+++ poker_server.py
@@ -2,3 +2,29 @@
 The `poker_server` module contains the business logic that allows the server
 to communicate with poker clients and allow gameplay.
 '''
+
+def main():
+    # Parse command line arguments
+    # Verify valid arguments
+    # Wait for `start` from a player and set-up game
+    # Wait for players to join and notify waiting players
+    # Start game
+    # Wait for antes
+    # Deal cards to all players
+    # Set first player turn
+    # Repeat:
+    #   Notify player, wait for choice
+    #   Handle choice
+    #   Cycle turn
+    #   Until betting ends
+    # Notify betting round over
+    # Wait for clients to choose cards to swap
+    #   assuming more than one player still in the game
+    # Second betting round
+    # Get remaining players hands and evaluate winner
+    # Notify players of winner, show hands (unless all but one folded)
+    # Repeat steps until all players have left but one
+    pass
+
+if __name__ == '__main__':
+    main()
